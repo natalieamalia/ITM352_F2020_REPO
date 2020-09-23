b28600988edf630efa62c416c40309b80b0cf8cc
Natalie Amalia <70290749+natalieamalia@users.noreply.github.com>
2020-09-23 06:32:40 -1000

    Lab3
    Lab3

---
+++ Lab6/EX4.js
@@ -0,0 +1,4 @@
+n = 2;
+age = 20;
+console.log (n = n + 54, n += age);
+console.log (n = n +z)
---
+++ Lab6/EX5.js
@@ -0,0 +1,4 @@
+first_name = 'Natalie';
+last_name = 'Amalia';
+full_name = '${first_name} + '' + ${last_name}'
+console.log(full_name)
